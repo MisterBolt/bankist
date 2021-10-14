a98348d3bba38c3231fd12f8ca1e2a6646ae07b8
MisterBolt <bielecki.karol.private@gmail.com>
2021-10-15 01:53:04 +0200

    Add modal window

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 "use strict";
+
+////////////////////---------- MODAL WINDOW ----------\\\\\\\\\\\\\\\\\\\\
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+
+function toggleModal() {
+  modal.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+}
+
+btnsOpenModal.forEach(btn => btn.addEventListener("click", toggleModal));
+
+btnCloseModal.addEventListener("click", toggleModal);
+overlay.addEventListener("click", toggleModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    toggleModal();
+  }
+});
